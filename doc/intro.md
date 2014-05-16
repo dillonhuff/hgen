74d18eb188d079a5352baa7a738c3cb74cd27feb
Dillon Huff <dillonhuff@gmail.com>
2014-05-16 10:11:10 -0700

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hgen
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/hgen/core_test.clj
@@ -0,0 +1,7 @@
+(ns hgen.core-test
+  (:require [clojure.test :refer :all]
+            [hgen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
